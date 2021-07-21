08233f564f82f979d8d098a216837459ca478777
Valentin <qweqweq@qwe>
2021-07-21 20:14:52 +0300

    initial commit

---
+++ src/main/java/com/vvvxdd/sweater_java/SweaterJavaApplication.java
@@ -0,0 +1,13 @@
+package com.vvvxdd.sweater_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweaterJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vvvxdd/sweater_java/SweaterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvvxdd.sweater_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
